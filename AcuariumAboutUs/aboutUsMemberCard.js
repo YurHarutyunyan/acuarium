1631b9f323cf861e124a1b6ea6cd13ebcbbe35cb
Margaritta2023 <margarittamkn@gmail.com>
2023-12-02 00:30:37 +0400

    add About Us page (index, logic, style files)

---
+++ AcuariumAboutUs/aboutUsMemberCard.js
@@ -0,0 +1,34 @@
+function memberCard(parent,name,containerId,className, imageUrl, gitUrl){
+     
+    const container = createElement("div");
+    createId(container, containerId);
+    container.className = "memberCard-container";
+    parent.append(container)
+
+    if (imageUrl) {
+        const imageElement = createElement("img");
+        createId(imageElement, containerId + "_Image");
+        imageElement.src = imageUrl;
+        container.append(imageElement);
+        imageElement.className = "imageCardFormat"
+      }
+
+      if (name) {
+        const nameElement = createElement("div");
+        createId(nameElement, containerId + "_Name");
+        createInnerHtml(nameElement, `<h4>${name}</h4>`);
+        container.append(nameElement);
+      }
+
+
+     if (gitUrl) {
+      const gitElement = createElement("a");
+      createId(gitElement, containerId + "_gitLink");
+      gitElement.className = "link";
+      gitElement.innerHTML = "<h6>GitHub</h6>";
+      gitElement.href = gitUrl;
+      container.append(gitElement);
+     
+    }
+    
+}
---
+++ AcuariumAboutUs/aboutUsPart2.js
@@ -0,0 +1,120 @@
+let section2 = createElement("section");
+createId(section2,"section2_container");
+
+rootId.append(section2);
+
+let section2Div1 = createElement("div");
+createId(section2Div1,"section2Div1");
+section2.append(section2Div1)
+
+createInnerHtml(section2Div1,"<h1 > Our Team <h1> ")
+createInnerHtml(section2Div1,"<h4>Introducing our team members</h4>")
+
+let section2Div2 = createElement("div");
+createId(section2Div2,"section2Div2");
+section2.append(section2Div2);
+
+memberCard(section2Div2,"Daniel Tadevosyan","section2Div2_container1", "memberCardContainer","./images/Daniel.jpg", "https://github.com/danieltadevosyan")
+memberCard(section2Div2,"Yuri Harutyunyan","section2Div2_container2", "memberCardContainer","./images/Yura.png", "https://github.com/YurHarutyunyan")
+memberCard(section2Div2,"Harutyun Ghazaryan","section2Div2_container3", "memberCardContainer","./images/Harut.png", "https://github.com/HarutyunGhazaryan")
+memberCard(section2Div2,"Margarita Mkrtchyan","section2Div2_container4", "memberCardContainer","./images/Maga.png", "https://github.com/Margaritta2023")
+
+
+// Daniel
+// const section2Div2_1Container = createElement("div")
+// createId(section2Div2_1Container,"section2Div2_1Container");
+// section2.append(section2Div2)
+
+// section2Div2.append(section2Div2_1Container)
+
+// let section2Div2_1Image = createElement("div");
+// createId(section2Div2_1Image,"section2Div2_1Image");
+
+// let section2Div2_1Name = createElement("div");
+// createId(section2Div2_1Name,"section2Div2_1Name");
+// createInnerHtml(section2Div2_1Name,"<h5> Daniel Tadevosyan <h5> ")
+
+// let section2Div2_1Link = createElement("a");
+// createId(section2Div2_1Link, "section2Div2_1Link");
+// section2Div2_1Link.innerHTML = "<h6>GitHub</h6>";
+// section2Div2_1Link.href = "https://github.com/danieltadevosyan";
+
+
+// section2Div2_1Container.append(section2Div2_1Image, section2Div2_1Name, section2Div2_1Link);
+
+// // Yura
+
+// const section2Div2_2Container = createElement("div")
+// createId(section2Div2_2Container,"section2Div2_2Container");
+
+// section2Div2.append(section2Div2_2Container)
+
+// let section2Div2_2Image = createElement("div");
+// createId(section2Div2_2Image,"section2Div2_2Image");
+
+// let section2Div2_2Name = createElement("div");
+// createId(section2Div2_2Name,"section2Div2_2Name");
+// createInnerHtml(section2Div2_2Name,"<h5> Yuri Harutyunyan <h5> ")
+
+// let section2Div2_2Link = createElement("a");
+// createId(section2Div2_2Link, "section2Div2_2Link");
+// section2Div2_2Link.innerHTML = "<h6>GitHub</h6>";
+// section2Div2_2Link.href = "https://github.com/YuriHarutyunyan";
+
+
+// section2Div2_2Container.append(section2Div2_2Image, section2Div2_2Name, section2Div2_2Link);
+
+// Harut
+
+
+// const section2Div2_3Container = createElement("div")
+// createId(section2Div2_3Container,"section2Div2_3Container");
+
+// section2Div2.append(section2Div2_3Container)
+
+// let section2Div2_3Image = createElement("div");
+// createId(section2Div2_3Image,"section2Div2_3Image");
+
+// let section2Div2_3Name = createElement("div");
+// createId(section2Div2_3Name,"section2Div2_3Name");
+// createInnerHtml(section2Div2_3Name,"<h5> Harutyun Ghazaryan <h5> ")
+
+// let section2Div2_3Link = createElement("a");
+// createId(section2Div2_3Link, "section2Div2_3Link");
+// section2Div2_3Link.innerHTML = "<h6>GitHub</h6>";
+// section2Div2_3Link.href = "https://github.com/HarutyunGhazaryan";
+
+
+// section2Div2_3Container.append(section2Div2_3Image, section2Div2_3Name, section2Div2_3Link);
+
+// Maga
+
+// const section2Div2_4Container = createElement("div")
+// createId(section2Div2_4Container,"section2Div2_4Container");
+// 
+
+// section2Div2.append(section2Div2_4Container)
+
+// let section2Div2_4Image = createElement("div");
+// createId(section2Div2_4Image,"section2Div2_4Image");
+
+// let section2Div2_4Name = createElement("div");
+// createId(section2Div2_4Name,"section2Div2_4Name");
+// createInnerHtml(section2Div2_4Name,"<h5> Margarita Mkrtchyan <h5> ")
+
+// let section2Div2_4Link = createElement("a");
+// createId(section2Div2_4Link, "section2Div2_4Link");
+// section2Div2_4Link.innerHTML = "<h6>GitHub</h6>";
+// section2Div2_4Link.href = "https://github.com/Margaritta2023";
+
+
+// section2Div2_4Container.append(section2Div2_4Image, section2Div2_4Name, section2Div2_4Link);
+
+//memebers end
+
+
+
+
+
+
+
