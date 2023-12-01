9696960ea32efa57b764d6b1d9edc9996713a2ed
Your Name <you@example.com>
2023-12-01 22:38:39 +0400

    done with acuarium

--- src/acuarium/acuariumPiece.js
+++ src/acuarium/acuariumPiece.js
@@ -3,7 +3,7 @@ function runNemo() {
     const imageWidth = image.width;
     const imageHeight = image.height;
     const newWidth = Math.random() * (700);
-    const newHeight = Math.random() * (450);
+    const newHeight = Math.random() * (250);
 
     image.style.transform = `translate(${newWidth}px, ${newHeight}px)`;
   }
